bae4f5e17a89ca9b237e326d7456471514bdebb2
Danilo Martinelli <danilo.martinelli@rdstation.com>
2022-03-22 13:57:06 -0300

    initial upload

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+end
